29d2e5aa66f269f86ae0eb10d48a2191ead6056a
Diego Enriquez <44416811+Diegomca98@users.noreply.github.com>
2026-03-28 22:29:29 -0600

    infra(terraform): Restructured for script management - (Created backend.tf | provider.tf | terraform.tf) (#4)

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_version = ">= 1.7.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  backend "s3" {
-    bucket = "xoloforge-tfstate-bkt"
-    key = "dev/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "xoloforge-tf-locks"
-    encrypt = true
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-  default_tags {
-    tags = local.common_tags
-  }
-}
-
 locals {
   environment = "dev"
   common_tags = {
